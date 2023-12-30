c6ca59a2397f46a2981e23469638b9829097367b
samcab28 <samircabrera2528@gmail.com>
2023-12-30 14:19:55 -0600

    creacion grafo

--- ProyectoAnalisisProgra/src/main/java/general/Main.java
+++ ProyectoAnalisisProgra/src/main/java/general/Main.java
@@ -1,50 +1,46 @@
 package general;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Your code goes here
         Scanner scanner = new Scanner(System.in);
 
-        int V = scanner.nextInt(); //cantidad de nodos
+        int V = scanner.nextInt(); // cantidad de nodos
+        int E = scanner.nextInt(); // cantidad de lineas
 
-        /*
-        la variable E indica el viaje de los nodos
-        primer numero = nodo inicio
-        segundo numero = nodo final
-        tercer numero = costo de viaje
+        // Crear un mapa para representar el grafo
+        Map<Integer, ArrayList<Integer[]>> grafo = new HashMap<>();
 
-        1. pedir la cantidad de lineas
-        2. almacenar los datos en un arraylist de arraylist, datos
-        mantendran el esquema mencionado
-
-        recordar que es un grafo no dirigido
-         */
-
-        int E = scanner.nextInt(); //cantidad de lineas
-
-        ArrayList<ArrayList> viajes = new ArrayList<>(); //arraylist para mantener la informacion de viajes
-
-        for(int i = 0; i<E; i++){
-            ArrayList<Integer> datosViaje = new ArrayList<>();
+        for (int i = 0; i < E; i++) {
             int nodoOrigen = scanner.nextInt();
             int nodoDestino = scanner.nextInt();
             int costoViaje = scanner.nextInt();
 
-            datosViaje.add(nodoOrigen);
-            datosViaje.add(nodoDestino);
-            datosViaje.add(costoViaje);
+            // Agregar el nodo de destino y el costo al nodo de origen
+            grafo.computeIfAbsent(nodoOrigen, k -> new ArrayList<>())
+                    .add(new Integer[]{nodoDestino, costoViaje});
 
-            viajes.add(datosViaje);
+            // Agregar el nodo de origen y el costo al nodo de destino
+            grafo.computeIfAbsent(nodoDestino, k -> new ArrayList<>())
+                    .add(new Integer[]{nodoOrigen, costoViaje});
         }
 
-        System.out.println(viajes);
+        // Imprimir el grafo
+        System.out.println("Representación del grafo:");
+        for (Map.Entry<Integer, ArrayList<Integer[]>> entry : grafo.entrySet()) {
+            int nodo = entry.getKey();
+            ArrayList<Integer[]> conexiones = entry.getValue();
 
-
-
-
+            System.out.print(nodo + ": ");
+            for (Integer[] conexion : conexiones) {
+                System.out.print("(" + conexion[0] + ", " + conexion[1] + ") ");
+            }
+            System.out.println();
+        }
     }
 }
