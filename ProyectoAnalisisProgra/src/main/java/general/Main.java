a828001af95c1912550cea604c49cd0c859fefa5
samcab28 <samircabrera2528@gmail.com>
2023-12-30 14:55:21 -0600

    armado con matriz

--- ProyectoAnalisisProgra/src/main/java/general/Main.java
+++ ProyectoAnalisisProgra/src/main/java/general/Main.java
@@ -1,9 +1,9 @@
 package general;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Main {
 
@@ -13,34 +13,44 @@ public class Main {
         int V = scanner.nextInt(); // cantidad de nodos
         int E = scanner.nextInt(); // cantidad de lineas
 
-        // Crear un mapa para representar el grafo
-        Map<Integer, ArrayList<Integer[]>> grafo = new HashMap<>();
+        // Crear una matriz de adyacencia para representar el grafo
+        int[][] grafo = construirGrafo(scanner, E, V);
+
+        // Imprimir la representación del grafo
+        imprimirGrafo(grafo);
+
+    }
+
+    // Función para construir la matriz de adyacencia del grafo
+    private static int[][] construirGrafo(Scanner scanner, int E, int V) {
+        int[][] grafo = new int[V][V];
 
         for (int i = 0; i < E; i++) {
             int nodoOrigen = scanner.nextInt();
             int nodoDestino = scanner.nextInt();
             int costoViaje = scanner.nextInt();
 
-            // Agregar el nodo de destino y el costo al nodo de origen
-            grafo.computeIfAbsent(nodoOrigen, k -> new ArrayList<>())
-                    .add(new Integer[]{nodoDestino, costoViaje});
-
-            // Agregar el nodo de origen y el costo al nodo de destino
-            grafo.computeIfAbsent(nodoDestino, k -> new ArrayList<>())
-                    .add(new Integer[]{nodoOrigen, costoViaje});
+            // Agregar la conexión al grafo
+            grafo[nodoOrigen - 1][nodoDestino - 1] = costoViaje;
+            grafo[nodoDestino - 1][nodoOrigen - 1] = costoViaje; // Considerando un grafo no dirigido
         }
 
-        // Imprimir el grafo
-        System.out.println("Representación del grafo:");
-        for (Map.Entry<Integer, ArrayList<Integer[]>> entry : grafo.entrySet()) {
-            int nodo = entry.getKey();
-            ArrayList<Integer[]> conexiones = entry.getValue();
+        return grafo;
+    }
 
-            System.out.print(nodo + ": ");
-            for (Integer[] conexion : conexiones) {
-                System.out.print("(" + conexion[0] + ", " + conexion[1] + ") ");
+    // Función para imprimir la representación del grafo
+    private static void imprimirGrafo(int[][] grafo) {
+        System.out.println("Representación del grafo:");
+
+        for (int i = 0; i < grafo.length; i++) {
+            System.out.print((i + 1) + ": ");
+            for (int j = 0; j < grafo[i].length; j++) {
+                System.out.print(grafo[i][j] + " ");
             }
             System.out.println();
         }
     }
+
+
+
 }
