e9a5c08e0cc710b78df62edd17d5043290488acb
samcab28 <samircabrera2528@gmail.com>
2024-01-04 14:47:38 -0600

    pregunta3 examen1

--- ProyectoAnalisisProgra/src/main/java/examen/Pregunta3.java
+++ ProyectoAnalisisProgra/src/main/java/examen/Pregunta3.java
@@ -1,5 +1,12 @@
 package examen;
 
+/*
+estudiante: Samir Cabrera
+
+complejidad:  O(n log n)
+ */
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -21,24 +28,30 @@ public class Pregunta3 {
         combos.add(5);
         combos.add(6);
 
-        int G = 70;
+        int G = 50;
 
-        // Calcular todas las combinaciones de precios por productos
         ArrayList<Integer> combinaciones = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < C; j++) {
                 combinaciones.add(precios.get(i) * combos.get(j));
             }
         }
-
-        // Ordenar las combinaciones en orden ascendente
         Collections.sort(combinaciones);
 
-        // Busqueda binaria para encontrar la combinación más pequeña mayor o igual a G
+        ArrayList<Integer> tablaAcumulada = new ArrayList<>();
+        int sumaAcumulada = 0;
+
         int resultado = binarySearch(combinaciones, G);
 
         if (resultado != -1) {
             System.out.println("La menor combinación es: " + resultado);
+            for (int combinacion : combinaciones) {
+                if (combinacion >= resultado) {
+                    sumaAcumulada += combinacion;
+                    tablaAcumulada.add(sumaAcumulada);
+                }
+            }
+            System.out.println("resultado: " + tablaAcumulada.get(tablaAcumulada.size()-1));
         } else {
             System.out.println("No hay combinación que sea mayor o igual a " + G);
         }
