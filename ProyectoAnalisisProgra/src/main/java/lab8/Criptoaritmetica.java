f0d864d6f9e4502f3b261f8a624f32a739fdf146
Mynell2022 <99918599+Mynell2022@users.noreply.github.com>
2024-01-05 10:44:33 -0600

    Update Criptoaritmetica.java
    Me pase un poco con codigo, pero funciona

--- ProyectoAnalisisProgra/src/main/java/lab8/Criptoaritmetica.java
+++ ProyectoAnalisisProgra/src/main/java/lab8/Criptoaritmetica.java
@@ -2,6 +2,10 @@ package lab8;
 
 //voraz
 //Falta todas las probabilidades posibles
+/* 
+Mynell Myers Hall
+Complejidad: O(n!)
+*/
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,7 +24,7 @@ public class Main {
         List<Character> abced = IntStream.rangeClosed('A', 'Z').mapToObj(c -> (char) c).collect(Collectors.toList());
         p1=p1.toUpperCase();p2=p2.toUpperCase();p3=p3.toUpperCase();
         List<Character> letras=new ArrayList<>();
-        List<Character> valLetras=new ArrayList<>();
+        List<Integer> valLetras=new ArrayList<>();
         Integer[] valores = new Integer[abced.size()];
         Arrays.fill(valores, 0);
         for(int i=0; i<p1.length();i++){
@@ -47,22 +51,51 @@ public class Main {
                 System.out.println("No es Posible");
                 return;
             }
+            if (!letras.contains(p1.charAt(i))) {letras.add(p1.charAt(i));valLetras.add(valores[valor1]);}
+            if (!letras.contains(p2.charAt(i))) {letras.add(p2.charAt(i));valLetras.add(valores[valor2]);}
         }
-        for(int i=0; i<p1.length();i++){
-            System.out.print(valores[abced.indexOf(p1.charAt(i))]);
-        }System.out.println();
-        for(int i=0; i<p2.length();i++){
-            System.out.print(valores[abced.indexOf(p2.charAt(i))]);
-        }System.out.println();
-        for(int i=0; i<p3.length();i++){
-            System.out.print(valores[abced.indexOf(p3.charAt(i))]);
-        }System.out.println();
-        System.out.println();
+        List<List<Integer>> permutaciones = new ArrayList<>();
+        permutar(valLetras,letras,p1,p2, 0, permutaciones);
+        for(List<Integer> valor:permutaciones){
+            for(int j=1;j<5;j++){
+                List<Integer> copia= new ArrayList<>(valor);
+                int c=j;
+                copia=copia.stream().map(elemento -> elemento * c).collect(Collectors.toList());
+                if(copia.stream().anyMatch(numero -> numero > 9)){break;}
+                for(int i=0; i<p1.length();i++){
+                    System.out.print(copia.get(letras.indexOf(p1.charAt(i))));
+                }System.out.println();
+                for(int i=0; i<p2.length();i++){
+                    System.out.print(copia.get(letras.indexOf(p2.charAt(i))));
+                }System.out.println();
+                for(int i=0; i<p3.length();i++){
+                    System.out.print(copia.get(letras.indexOf(p1.charAt(i)))+copia.get(letras.indexOf(p2.charAt(i))));
+                }System.out.println();
+                System.out.println();
+            }
+        }
     }
     public static int encontrarValor(int p1, int p2, int p3) {
         if (p1 == 0) {return p3 - p2;
         }if (p2 == 0) {return p3 - p1;
         }if (p3 == 0) {return p1 + p2;
         }return -1;
+    }
+    public static void permutar(List<Integer> elementos,List<Character> letras,String p1,String p2, int indice, List<List<Integer>> permutaciones) {
+        if (indice == elementos.size() - 1 ) {
+            boolean con=true;
+            for(int i=0; i<p1.length();i++){
+                int valor1= letras.indexOf(p1.charAt(i));
+                int valor2= letras.indexOf(p2.charAt(i));
+                if(encontrarValor(elementos.get(valor1),elementos.get(valor2),0)>9){con=false;break;}
+            }
+            if(con){permutaciones.add(new ArrayList<>(elementos));
+            return;}
+        }
+        for (int i = indice; i < elementos.size(); i++) {
+            Collections.swap(elementos, indice, i);
+            permutar(elementos,letras,p1,p2, indice + 1, permutaciones);
+            Collections.swap(elementos, indice, i);
+        }
     }
 }
